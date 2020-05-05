1dbb870ef302c458e75c6ec3876c6982e4972c4f
ALongTail <56359558+ALongTail@users.noreply.github.com>
2020-05-05 20:27:54 +0800

    Update dbconnect.cpp

--- dbconnect.cpp
+++ dbconnect.cpp
@@ -14,7 +14,7 @@ dbconnect::dbconnect(){
                                                 "周一 varchar,周二 varchar,周三 varchar,周四 varchar,周五 varchar)");
         bool table3=query.exec("create table if not exists homework(Date varchar primary key,chinese varchar,math varchar,english varchar,"
                                                 "physics varchar,chemistry varchar,biology varchar,politics varchar,history varchar,"
-                                                "geology varchar,elseall varchar)");
+                                                "geology varchar)");
         if(table1&&table2&&table3)qDebug()<<"tables";
 }
 
@@ -35,8 +35,8 @@ void dbconnect::insert(){
     QSqlDatabase db=QSqlDatabase::database("sqlite");
     int i;
     QSqlQuery query(db);
-    query.prepare(QString("REPLACE INTO homework(Date,chinese,math,english,physics,chemistry,biology,politics,history,geography,elseall)"
-                          "VALUES(?,?,?,?,?,?,?,?,?,?,?)"));
+    query.prepare(QString("REPLACE INTO homework(Date,chinese,math,english,physics,chemistry,biology,politics,history,geography)"
+                          "VALUES(?,?,?,?,?,?,?,?,?,?)"));
     for(i=0;i<10;i++)
         query.bindValue(i,bond[i]);
     query.exec();
@@ -47,24 +47,20 @@ QString dbconnect::clean_search(){
     QSqlDatabase db=QSqlDatabase::database("sqlite");
     QSqlQuery query(db);
     QDate day;
-    const QString date=day.currentDate().toString("ddd");
-        qDebug()<<date;
+    const QString DATE=day.currentDate().toString("ddd");
     QString StuOnDuty;
-    query.exec("select * from students where day='&trim(date)&'");
-    QString s;
-    QSqlRecord rec=query.record();qDebug()<<rec.count();
+    QString toExec="select * from students where day='"+DATE.trimmed()+"'";
+        if(query.exec(toExec))qDebug()<<"ok";
     while(query.next()){
-        s+=query.value(1).toString()+query.value(4).toString()+"\n";
-        qDebug()<<s;
-    }
-    return s;
-//todo: debug
+        StuOnDuty+=query.value(1).toString()+query.value(4).toString()+"\n";
+    }qDebug()<<StuOnDuty;
+    return StuOnDuty;
 }
 
 void dbconnect::class_display(){
     QSqlDatabase db=QSqlDatabase::database("sqlite");
     QSqlQuery query(db);
     query.exec("select * from classes");
-//todo: display today's class and their start and over time in labels
+//todo:这跟你的设计有关，交给你了
 }
 
